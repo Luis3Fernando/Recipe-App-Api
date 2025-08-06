37a020bea9bfa1c407b50ea12d12509648003ed8
Luis Fernando Chumbes Ramos <luisfernando3chr@gmail.com>
2025-08-05 23:37:04 -0500

    feat: Creating tests for ingredient

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -76,3 +76,13 @@ class ModelTests(TestCase):
         tag = models.Tag.objects.create(user=user, name='Tag1')
         
         self.assertEqual(str(tag), tag.name)
+        
+    def test_create_igredient(self):
+        """Test creating an ingrediente is succesful"""
+        user = create_user()
+        ingredient = models.Ingredient.objects.create(
+            user=user,
+            name="Ingrediant1"
+        )
+        
+        self.assertEqual(str(ingredient), ingredient.name)
